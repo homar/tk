e3568ea28a7fcd48e7b0ef03296973d29cf29d86
Rafal Fronczyk <rfronczyk@gmail.com>
2009-01-28 12:13:33 +0100

    Added my_print.c

---
+++ my_print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void my_print(char* string) {
+  printf("%s", string);
+} 
+
+void my_println(char* string) {
+  printf("%s\n", string);
+}
+
+void my_int_print(int a) {
+  printf("%d", a);
+}
+
+void my_int_println(int a) {
+  printf("%d\n", a);
+}
